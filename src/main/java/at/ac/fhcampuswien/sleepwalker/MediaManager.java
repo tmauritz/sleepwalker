4475ce7004c6ce976874eb579dd939a3bf0c9efd
Stefan Lackovic <stefan.lackovic@stud.fh-campuswien.ac.at>
2023-12-26 20:37:30 +0100

    Added Class MediaManager. Added idea for HowToPlay Menu.

---
+++ src/main/java/at/ac/fhcampuswien/sleepwalker/MediaManager.java
@@ -0,0 +1,4 @@
+package at.ac.fhcampuswien.sleepwalker;
+
+public class MediaManager {
+}
--- src/main/java/at/ac/fhcampuswien/sleepwalker/controller/HowToPlayController.java
+++ src/main/java/at/ac/fhcampuswien/sleepwalker/controller/HowToPlayController.java
@@ -1,10 +1,19 @@
 package at.ac.fhcampuswien.sleepwalker.controller;
 
 import at.ac.fhcampuswien.sleepwalker.GameManager;
+import at.ac.fhcampuswien.sleepwalker.Sleepwalker;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HowToPlayController {
 
     public void showBackToMainMenu() {
         GameManager.getInstance().showMainMenu();
     }
+
 }
