c943c18cdbb785df38333067268d6ddb9ca14f4d
Stefan Lackovic <stefan.lackovic@stud.fh-campuswien.ac.at>
2024-01-18 15:58:20 +0100

    added Settings.fxml and SettingsController.

--- src/main/java/at/ac/fhcampuswien/sleepwalker/controller/MainMenuController.java
+++ src/main/java/at/ac/fhcampuswien/sleepwalker/controller/MainMenuController.java
@@ -69,8 +69,7 @@ public class MainMenuController implements Initializable {
         //switch to WorldMap Scene
         GameManager.getInstance().showWorldMap();
     }
-    public void showSettingsButton() {
-
+    public void showSettingsButton(ActionEvent actionEvent) {
     }
 
     /**
